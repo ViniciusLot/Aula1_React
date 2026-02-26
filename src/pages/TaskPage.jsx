9ef82cfbe2f3fca59a9b049579ff17f516186be0
ViniciusLot <vinicius.rizzo.lot@gmail.com>
2026-02-26 10:07:14 -0300

    versao_linear

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -10,7 +10,7 @@ function TaskPage() {
 
   return (
     <div className="w-screen h-screen bg-slate-500 p-6">
-      <div className="w-[500px] space-y-4">
+      <div className="w-[500px] mx-auto space-y-4">
         <div className = "flex justify-center relative"> 
         <button onClick={() => navigate(-1)} className="absolute left-0 top-0 bottom-0 mb-6 text-slate-100">
             <ChevronLeftIcon />
